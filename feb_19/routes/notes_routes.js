01628a5596c00c5d601caddf625aecb77d884a96
toastynerd <tylermorgan86@gmail.com>
2015-02-19 13:17:14 -0800

    start point

---
+++ feb_19/routes/notes_routes.js
@@ -0,0 +1,34 @@
+'use strict';
+var Note = require('../models/Note');
+var bodyparser = require('body-parser');
+
+module.exports = function(app) {
+  app.use(bodyparser.json());
+
+  app.get('/notes', function(req, res) {
+    Note.find({}, function(err, data) {
+      if (err) return res.status(500).send({'msg': 'could not retrieve notes'});
+
+      res.json(data);
+    });
+  });
+
+  app.post('/notes', function(req, res) {
+    var newNote = new Note(req.body); 
+    newNote.save(function(err, note) {
+      if (err) return res.status(500).send({'msg': 'could not save note'});
+
+      res.json(note);
+    });
+  });
+
+  app.put('/notes/:id', function(req, res) {
+    var updatedNote = req.body;
+    delete updatedNote._id;
+    Note.update({_id: req.params.id}, updatedNote, function(err) {
+      if (err) return res.status(500).send({'msg': 'could not update note'});
+
+      res.json(req.body);
+    });
+  });
+};
