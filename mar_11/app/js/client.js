122a09b99f0e9651e0ab2c49cbbf4ab326f18504
toastynerd <tylermorgan86@gmail.com>
2015-03-11 14:12:51 -0700

    before ajax form submission

--- mar_11/app/js/client.js
+++ mar_11/app/js/client.js
@@ -4,6 +4,29 @@ var React = require('react');
 
 var notesData = [{noteBody: 'hello world', _id: 1}, {noteBody: 'goodbye world', _id: 2}];
 
+var NoteForm = React.createClass({
+  getInitialState: function() {
+    return {newNote: {noteBody: ''}};
+  },
+  handleChange: function(event) {
+    this.setState({newNote: {noteBody: event.target.value}});
+  },
+  handleSubmit: function(event) {
+    event.preventDefault();
+    this.props.onNewNoteSubmit(this.state.newNote);
+    this.setState({newNote: {noteBody: ''}});
+  },
+  render: function() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor="newnote">New Note</label>
+        <input id="newnote" type="text" value={this.state.newNote.noteBody} onChange={this.handleChange}/>
+        <button type="submit">Create New Note</button>
+      </form>
+    )
+  }
+});
+
 var Note = React.createClass({
   render: function() {
     return <li>{this.props.data.noteBody}</li>
@@ -12,10 +35,9 @@ var Note = React.createClass({
 
 var NoteList = React.createClass({
   render: function() {
-    var notes = [];
-    for(var i = 0; i < this.props.data.length; i++){
-      notes.push(<Note data={this.props.data[i]} key={this.props.data[i]._id}/>);
-    }
+    var notes = this.props.data.map(function(note) {
+      return <Note data={note} key={note._id}/>
+    });
     return (
       <section>
         <h1>Notes:</h1>
@@ -31,9 +53,16 @@ var App = React.createClass({
   getInitialState: function() {
     return {notesData: notesData};
   },
+  onNewNote: function(note) {
+    note._id = this.state.notesData.length + 1;
+    var stateCopy = this.state;
+    stateCopy.notesData.push(note);
+    this.setState(stateCopy);
+  },
   render: function() {
     return (
       <main>
+        <NoteForm onNewNoteSubmit={this.onNewNote}/>
         <NoteList data={this.state.notesData} />
       </main>
     )
