a6e512f073ae2ccb4f69386f173b0e0bbf102301
toastynerd <tylermorgan86@gmail.com>
2015-03-17 11:56:11 -0700

    further changes to start of day

--- mar_17/server.js
+++ mar_17/server.js
@@ -2,17 +2,25 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var notesRoutes = require('./routes/notes_routes');
+var passport = require('passport');
 
 mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost/notesapp_development');
 
 var app = express();
 app.use(express.static(__dirname + '/build'));
 
-var router = express.Router();
+app.set('appSecret', process.env.SECRET || 'changethischangethis!');
+app.use(passport.initialize());
+require('./lib/passport_strat')(passport);
 
-notesRoutes(router);
+var notesRouter = express.Router();
+var userRouter = express.Router();
 
-app.use('/api/v1', router);
+notesRoutes(notesRouter, app.get('appSecret'));
+require('./routes/user_routes')(userRouter, passport, app.get('appSecret'));
+
+app.use('/api/v1', notesRouter);
+app.use('/api/v1', userRouter);
 
 app.listen(process.env.PORT || 3000, function() {
   console.log('server listening on port ' + (process.env.PORT || 3000));
